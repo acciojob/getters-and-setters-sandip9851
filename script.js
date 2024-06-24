b391c4133916bb7465916b904cf613165f7cf547
Sandip  Pradhan <158505928+sandip9851@users.noreply.github.com>
2024-06-24 21:19:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name, age){
+		this.name = name
+		this.age = age
+	}
+	get name(){
+		return this.name;
+	}
+	set age(age){
+		this.age = age
+	}
+}
 
-class Student extends Person {}
+class Student extends Person {
+	study(){
+		constructor(name){
+			super(name)
+		}
+		
+		console.log(this.name+"is studying")
+	}
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+	constructor(name){
+		super(name)
+	}
+	tech(){
+		console.log(this.name+"is studying")
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
