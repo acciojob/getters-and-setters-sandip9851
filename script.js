53229bd7466b31d2aca4b7327074234c167a222f
Sandip  Pradhan <158505928+sandip9851@users.noreply.github.com>
2024-06-25 21:36:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
 class Person {
 	constructor(name,age){
-    this.n=name
-    this.a=age
     this._name=name
     this._age=age
   }
-
+  
   get name(){
-    return this.n
     return this._name
   }
-
+  
   set age(age){
-    this.a=age
     this._age=age
   }
 }
+
+class Student extends Person {
+	study(){
+    console.log(this.name+" is studying")
+  }
+}
+
+class Teacher extends Person {
+	teach(){
+    console.log(this.name+" is teaching")
+  }
+}
+
+// Do not change the code below this line
+window.Person = Person;
+window.Student = Student;
+window.Teacher = Teacher;
